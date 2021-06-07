0d92b54d0835601733136440d5dc63b822eb0ba2
kevcode <kaesortega@gmail.com>
2021-06-06 20:56:38 -0500

    Todo ok

---
+++ src/main/java/com/platzimarket/persistence/ProductoRepository.java
@@ -0,0 +1,23 @@
+package com.platzimarket.persistence;
+
+import com.platzimarket.persistence.crud.ProductoCrudRepository;
+import com.platzimarket.persistence.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return  productoCrudRepository.findByIdCategoria(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+}
